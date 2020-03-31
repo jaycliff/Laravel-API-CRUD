6466c3f8839dc28c71361dc6a8c167cb93ff0266
Cliffmeister <jaycliff702@gmail.com>
2020-03-31 16:27:28 +0800

    wew

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'role' => 'nullable',
+            'information' => 'nullable'
+        ]);
+        $user = new User();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = Hash::make($data['password']);
+        $user->role = $data['role'] ?? 'member';
+        $user->information = $data['information'] ?? '';
+        $user->save();
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return $user;
+    }
+
+    public function isValid($value) {
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        // $data = $request->post();
+        $data = $request->validate([
+            'name' => 'nullable',
+            'email' => 'nullable|email',
+            'password' => 'nullable',
+            'role' => 'nullable',
+            'information' => 'nullable'
+        ]);
+        if (!empty($data['name'])) {
+            $user->name = $data['name'];
+        }
+        if (!empty($data['email'])) {
+            $user->email = $data['email'];
+        }
+        if (!empty($data['password'])) {
+            $user->password = Hash::make($data['password']);
+        }
+        if (!empty($data['role'])) {
+            $user->role = $data['role'];
+        }
+        if (!empty($data['information'])) {
+            $user->information = $data['information'];
+        }
+        $user->save();
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return $user;
+    }
+}
